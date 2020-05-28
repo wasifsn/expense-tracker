f8fabf579fbd97fdeb4521a61d7b05a455bf0430
wasifsn <wasif@piktorlabs.com>
2020-05-29 00:08:47 +0530

    complete ui functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncExp } from './components/IncExp';
+import { ExpenseList } from './components/ExpenseList';
+import './App.css';
+import { AddExpense } from './components/AddExpense';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
-	return <div>Hello</div>;
+	return (
+		<GlobalProvider>
+			<Header />
+			<div className='container'>
+				<Balance />
+				<IncExp />
+				<ExpenseList />
+				<AddExpense />
+			</div>
+		</GlobalProvider>
+	);
 }
 
 export default App;
